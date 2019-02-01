a9760f59c8dc4e83a36da7bf937b1ab5f7dac7ff
Scarpz <felipe.scarpz@gmail.com>
2019-02-01 16:02:26 -0200

    BaseURL created

---
+++ BeerMe/Utils/BaseURL.swift
@@ -0,0 +1,17 @@
+//
+//  BaseURL.swift
+//  BeerMe
+//
+//  Created by Scarpz on 01/02/19.
+//  Copyright © 2019 scarpz. All rights reserved.
+//
+
+import Foundation
+
+/// Struct to store all the URLs used in the application
+struct BaseURL {
+    static let getBeers = "https://api.punkapi.com/v2/beers?page={page}"
+    static let getBeerByName = "https://api.punkapi.com/v2/beers?beer_name={name}"
+    static let getBeerById = "https://api.punkapi.com/v2/beers/{id}"
+    static let getRandomBeer = "https://api.punkapi.com/v2/beers/random"
+}
