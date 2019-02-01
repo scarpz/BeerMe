cbf2c8b3d9a6b9162658fdd55c6b3dc4b8527f0b
Scarpz <felipe.scarpz@gmail.com>
2019-02-01 16:47:22 -0200

    Request class created

---
+++ BeerMe/Utils/RequestMethod.swift
@@ -0,0 +1,16 @@
+//
+//  RequestMethod.swift
+//  BeerMe
+//
+//  Created by Scarpz on 01/02/19.
+//  Copyright © 2019 scarpz. All rights reserved.
+//
+
+import Foundation
+
+/// Enum of HTTP request methods
+///
+/// - get: GET method
+enum RequestMethod: String {
+    case get = "GET"
+}
